23285d1092237b8894b90f4bf09bf23ae7e5ca7d
haota1307 <trananhhao1307@gmail.com>
2026-02-12 15:40:18 +0700

    chore(swagger): setup swagger

--- apps/bff/src/main.ts
+++ apps/bff/src/main.ts
@@ -7,6 +7,7 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,10 +18,29 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
+  // Swagger Documentation
+  const config = new DocumentBuilder()
+    .setTitle('Einvoice BFF Service API')
+    .setDescription('The Einvoice BFF Service API description')
+    .setVersion('1.0.0')
+    .addBearerAuth({
+      description: 'Default JWT Authorization',
+      type: 'http',
+      in: 'header',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+    })
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, documentFactory);
+
   await app.listen(port);
 
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    `📄 Documentation available at: http://localhost:${port}/${globalPrefix}/docs`,
   );
 }
 
