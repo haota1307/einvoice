58cdc0c50813735ce2f4ade1c1540d151e2ddb8b
haota1307 <trananhhao1307@gmail.com>
2026-03-12 08:41:20 +0700

    feat(user-guard): enhance token verification with caching mechanism and generate cache key

--- libs/guards/src/lib/user.guard.ts
+++ libs/guards/src/lib/user.guard.ts
@@ -6,8 +6,10 @@ import {
   Logger,
   Inject,
 } from '@nestjs/common';
-import { firstValueFrom, map, Observable } from 'rxjs';
+import { createHash } from 'node:crypto';
 import { Reflector } from '@nestjs/core';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { firstValueFrom, map, Observable } from 'rxjs';
 
 import { MetadataKeys } from '@common/constants/common.constants';
 import { getAccessToken, setUserData } from '@common/utils/request.until';
@@ -15,6 +17,7 @@ import { TCP_REQUEST_MESSAGE } from '@common/constants/enum/tcp-request-message.
 import { TCP_SERVICES } from '@common/configuration/tcp.config';
 import { TcpClient } from '@common/interfaces/tcp/common/tcp-client.interface';
 import { AuthorizeResponse } from '@common/interfaces/tcp/authorizer';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class UserGuard implements CanActivate {
@@ -24,6 +27,7 @@ export class UserGuard implements CanActivate {
     private readonly reflector: Reflector,
     @Inject(TCP_SERVICES.AUTHORIZER_SERVICE)
     private readonly authorizerClient: TcpClient,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
   ) {}
 
   canActivate(
@@ -46,8 +50,18 @@ export class UserGuard implements CanActivate {
   private async verifyToken(request: any): Promise<boolean> {
     try {
       const token = getAccessToken(request);
+      const cacheKey = this.generateTokenCacheKey(token);
+
       const processId =
         request[MetadataKeys.PROCESS_ID] || 'unknown-process-id';
+
+      const cacheData =
+        await this.cacheManager.get<AuthorizeResponse>(cacheKey);
+
+      if (cacheData) {
+        setUserData(request, cacheData);
+        return true;
+      }
 
       const result = await this.verifyUserToken(token, processId);
 
@@ -56,6 +70,8 @@ export class UserGuard implements CanActivate {
       }
 
       setUserData(request, result);
+
+      this.cacheManager.set(cacheKey, result, 30 * 60 * 1000);
 
       return true;
     } catch (error) {
@@ -75,5 +91,10 @@ export class UserGuard implements CanActivate {
         )
         .pipe(map((data) => data.data)),
     );
+  }
+
+  generateTokenCacheKey(token: string) {
+    const hash = createHash('sha256').update(token).digest('hex');
+    return `user-token:${hash}`;
   }
 }
